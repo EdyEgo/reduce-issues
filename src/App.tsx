f6c3154db8dc0dac4d07bdfd32992ddf5287dd27
septimiuProdanAdminTools <65479793+EdyEgo@users.noreply.github.com>
2022-05-17 13:20:11 +0300

    some structure god damn it :)

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {} from '@reduxjs/toolkit'
 import { useState ,useEffect } from 'react';
+import "./styles/signButtons.css";
 
 
 function App() {
