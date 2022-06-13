fc30ff29bf08de47a5a6f8b5fefaf73d84253400
EdyEgo <prodan.septimiu@gmail.com>
2022-06-13 14:24:08 +0300

    filter issues on grouping issues created

--- src/components/app-area/right-content/filtered-issues/issuesList/gruppingIssues.tsx
+++ src/components/app-area/right-content/filtered-issues/issuesList/gruppingIssues.tsx
@@ -1,4 +1,5 @@
 import {useSelector} from 'react-redux'
+import filterTeamIssues from '../../../../../services/issues/filterIssues'
 
 interface GrouppingIssuesProps {
     
@@ -7,6 +8,15 @@ interface GrouppingIssuesProps {
 const GrouppingIssues: React.FC<GrouppingIssuesProps> = () => {
    
     const viewFilters = useSelector((state:any)=>state.filtersIssues.viewFilters)
+    
+   const filtersListOrder  = useSelector((state:any)=>state.filtersIssues.filtersListOrder)
+
+    const selectedTeamId = useSelector((state:any)=>state.selectedTab.selectedTabAppArea.selectedTeamId)
+    const teamIssues = useSelector((state:any)=>state.issues.teamsIssues)
+    const selectedTeamIssues = teamIssues[selectedTeamId] != null ? teamIssues[selectedTeamId] : []
+     
+    const filteredIssuesList = filterTeamIssues(filtersListOrder,selectedTeamIssues)
+    console.log('you filter by this selectedTeamIssues',filteredIssuesList)
     const useDefaultViewFilters = viewFilters.custom.empty ? viewFilters.default : viewFilters.custom
     const listIsGrouped = useDefaultViewFilters.groupingBy !== "none" 
 
---
+++ src/services/issues/filterIssues.ts
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+export default function filterIssues(filtersListOrder:{[key:string]:any[]},selectedTeamIssues:any[]){
+   let filteredTeamIssues:any[] = []; 
+
+   for(let issueIndex= 0; issueIndex < selectedTeamIssues.length;issueIndex++){
+           const issue = selectedTeamIssues[issueIndex]
+           
+
+           let meetsPriority = true  
+           let meetsStatus = true  
+           let meetsLabel = true 
+           let meetsAssignee = true 
+           let meetsCreeator = true // :) nice name mate
+
+           // if issue.priority is null then the find index is gonna return -1 and its ok
+  
+           const priorityTransformFromNull = issue.priority === null ? {icon:""} : issue.priority
+           const labelTransformFromNull = issue.label === null ? {icon:""}  : issue.label 
+           const statusTransformFromNull = issue.status === null ? {icon:""}  : issue.status
+           if( filtersListOrder.priority.length >= 1 && filtersListOrder.priority.findIndex((selectedPriority)=>selectedPriority.value.icon === priorityTransformFromNull.icon) === -1){
+          
+              meetsPriority = false
+   
+           }
+           if(filtersListOrder.status.length >=1 && filtersListOrder.status.findIndex((selectedStatus)=>selectedStatus.value.icon === statusTransformFromNull.icon) === -1){
+              meetsStatus = false
+           }
+
+           if(filtersListOrder.labels.length >=1 && filtersListOrder.labels.findIndex((selectedLabel)=>selectedLabel.value.icon === labelTransformFromNull.icon) === -1){
+              meetsLabel = false
+           }
+
+           if(issue.assignedToUserId != null && filtersListOrder.assignee.length >= 1 && filtersListOrder.assignee.findIndex((selectedAssignee)=>selectedAssignee.userId === issue.assignedToUserId) === -1){
+              meetsAssignee = false
+           }
+
+           if(issue.creatorId != null  &&  filtersListOrder.creator.length >= 1 && filtersListOrder.creator.findIndex((selectedCreatorId)=>selectedCreatorId.userId === issue.creatorId) === -1){
+         
+             
+            meetsCreeator = false
+           }
+
+       // due date left
+
+           if(meetsPriority && meetsStatus && meetsLabel && meetsAssignee && meetsCreeator) filteredTeamIssues.push(issue)
+
+
+   }
+
+   return filteredTeamIssues
+
+}
